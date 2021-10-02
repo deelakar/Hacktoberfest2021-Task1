bc83469069b6fef32cc4f5b908a87137765dce72
Deelaka Rathnayake <deelaka98@gmail.com>
2021-10-03 00:02:55 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -4,15 +4,16 @@ int main()
 {
     char name,country,skill;
     int age;
-    printf("Welcome to Hacktoberfest 2020\n");
+    printf("Welcome to Hacktoberfest 2021\n");
     printf("Enter your name : ");//Get User name
     scanf("%c",&name);
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%c",&country);
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
+    scanf("%c",&age);
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf("%c",&skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    printf("Thank you %c Happy Hacking",country);//Thank the user
+    
     return 0;
 }
